c1c05c4bca5551519a25f3c39e85cd4b6011f1ee
王超 <chao198802@163.com>
2016-11-29 07:44:49 +0800

    网络请求

--- src/cn/shuitian/api/ResponseUtil.java
+++ src/cn/shuitian/api/ResponseUtil.java
@@ -13,4 +13,10 @@ public class ResponseUtil {
 		BaseResponse br = new BaseResponse();
 		response.getWriter().append(new Gson().toJson(br));
 	}
+	
+	public static void returnSuccess(HttpServletResponse response, Object data) throws IOException{
+		BaseResponse br = new BaseResponse();
+		br.data = data;
+		response.getWriter().append(new Gson().toJson(br));
+	}
 }
