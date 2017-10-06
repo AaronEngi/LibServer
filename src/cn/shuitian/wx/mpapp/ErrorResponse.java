2d894ad94ba2aa45950fb088a6e7ca49e4237ee2
王超 <343331640@qq.com>
2017-10-06 09:07:39 +0800

    清空

--- src/cn/shuitian/wx/mpapp/ErrorResponse.java
+++
@@ -1,6 +0,0 @@
-package cn.shuitian.wx.mpapp;
-
-public class ErrorResponse {
-	public int errcode;
-	public String errmsg;
-}
--- src/cn/shuitian/wx/mpapp/MpAppConstant.java
+++
@@ -1,6 +0,0 @@
-package cn.shuitian.wx.mpapp;
-
-public class MpAppConstant {
-	public static String AppId = "wxc855d041bcba89fc";
-	public static String AppSecret = "d37dea832d0fc98d21007067762e8d36";
-}
