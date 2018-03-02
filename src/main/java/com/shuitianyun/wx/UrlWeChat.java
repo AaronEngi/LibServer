4ca525375e86d4d2d3eb16d54dd79e2fc44c7da4
王超 <343331640@qq.com>
2018-03-02 10:42:00 +0800

    提供通用性

---
+++ src/main/java/com/shuitianyun/wx/UrlWeChat.java
@@ -0,0 +1,4 @@
+package com.shuitianyun.wx;
+
+public class UrlWeChat {
+}
--- src/main/java/com/shuitianyun/wx/mpapp/MpAppLoginApi.java
+++ src/main/java/com/shuitianyun/wx/mpapp/MpAppLoginApi.java
@@ -6,28 +6,32 @@ import java.io.IOException;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
-import wang.tyrael.library.http.HttpDefault;
-import wang.tyrael.library.log.LogAdapter;
 import okhttp3.Response;
+import wang.tyrael.http.HttpDefault;
+import wang.tyrael.log.LogAdapter;
 
 //https://mp.weixin.qq.com/debug/wxadoc/dev/api/api-login.html?t=20161122
 public class MpAppLoginApi {
 	public static final String UrlSession = 
 			"https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
-	private static final String TAG = "MpAppLoginManager";
-			
-	public static String getUrlSession(String code){
-		return String.format(UrlSession, MpAppConstant.AppId, MpAppConstant.AppSecret, code);
-	}
+	private static final String TAG = "MpAppLoginApi";
+
+	private final String appId;
+	private final String appSecret;
 	
 	private SessionResponse sessionResposne;
+
+	public MpAppLoginApi(String appId, String appSecret) {
+		this.appId = appId;
+		this.appSecret = appSecret;
+	}
 	
 	/**
 	 * 前端给出code，这就是登录过程.
 	 * 同步方法
 	 * @param code
 	 */
-	public SessionResponse toSession(String code){
+	public SessionResponse requestSession(String code){
 		String url = getUrlSession(code);
 		Response response= HttpDefault.get(url);
 		try {
@@ -40,5 +44,9 @@ public class MpAppLoginApi {
 			sessionResposne = null;
 		}
 		return sessionResposne;
+	}
+
+	private String getUrlSession(String code){
+		return String.format(UrlSession, appId, appSecret, code);
 	}
 }
